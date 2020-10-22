4a10804a7a71ecd806635f346c5932b2ab733ba4
ferial71 <ff_bouzoul@esi.dz>
2020-10-23 00:48:24 +0100

    added basic routes and test for book

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::post('books','BooksController@store')->name('books');
+
+Route::post('/books','BooksController@store');
+Route::patch('/books/{book}','BooksController@update');
+
--- tests/Feature/bookReservationTest.php
+++ tests/Feature/bookReservationTest.php
@@ -9,6 +9,7 @@ use Tests\TestCase;
 
 class bookReservationTest extends TestCase
 {
+    use RefreshDatabase;
   /** @test */
     public function a_book_can_be_added_to_the_library()
     {
@@ -19,5 +20,51 @@ class bookReservationTest extends TestCase
         ]);
         $response->assertOk();
         $this->assertCount(1,Book::all());
+    }
+    /** @test */
+    public function a_title_is_required()
+    {
+//        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => '',
+            'author' => 'ferial',
+        ]);
+
+        $response->assertSessionHasErrors('title');
+
+
+    }
+    /** @test */
+    public function an_author_is_required()
+    {
+//        $this->withoutExceptionHandling();
+        $response = $this->post('/books', [
+            'title' => 'title',
+            'author' => '',
+        ]);
+
+        $response->assertSessionHasErrors('author');
+
+
+    }
+    /** @test */
+    public function a_book_can_be_updated()
+    {
+        $this->withoutExceptionHandling();
+        $this->post('/books', [
+            'title' => 'title',
+            'author' => 'ferial',
+        ]);
+
+        $book = Book::first();
+
+        $response =$this->patch('/books/' . $book->id,[
+            'title' =>'new title',
+            'author' =>'new author',
+        ]);
+        $this->assertEquals('new title', Book::first()->title);
+        $this->assertEquals('new author', Book::first()->author);
+
+
     }
 }
